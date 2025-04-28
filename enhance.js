09ed76cb8deeb91f07c2453bd5701ea4859b2438
Niko Diamadis <cyb3rko@pm.me>
2025-04-28 02:44:18 +0200

    feat: colorize ads and enhance OSM button styling

--- enhance.js
+++ enhance.js
@@ -1,12 +1,15 @@
-function enhance() {
+function enhanceOnLoad() {
   // hide our referral, so startpage does not get problems with our extension :)
   hideReferrer();
-  // Set search-title and return query
-  const searchTerm = setTitle();
+  // set search title and return query
+  return setTitle();
+}
+
+function enhanceDelayed(searchTerm) {
   // Append OSM link
-  if (searchTerm !== undefined && searchTerm !== "") {
-    addOsmLink(searchTerm);
-  }
+  addOsmLink(searchTerm)
+  // colorize ads in results
+  colorizeAds();
 }
 
 function hideReferrer() {
@@ -14,6 +17,34 @@ function hideReferrer() {
   meta.name = "referrer";
   meta.content = "no-referrer";
   document.getElementsByTagName("head")[0].appendChild(meta);
+}
+
+function colorizeAds() {
+  let color = window.getComputedStyle(document.documentElement).background;
+  if (color.substring(0, 3) !== "rgb") {
+    console.error("Input color not in expected rgb() format.");
+    return;
+  }
+  const rgb = color.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  let ratio;
+  if ((rgb[1] * 299 + rgb[2] * 587 + rgb[3] * 114) / 1000 > 128) {
+    // Bright background, use darker color
+    ratio = 0.93;
+  } else {
+    // Dark background, use brighter color
+    ratio = 1.5;
+  }
+  for (let i = 0; i < 3; i++) {
+    const int = parseInt(rgb[i + 1]);
+    rgb[i] = Math.max(0, Math.min(255, Math.floor(int * ratio)));
+  }
+  color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+
+  const ads = document.getElementsByClassName("right-side-attribution-container")
+  console.log(`Found ${ads.length} ads to colorize.`);
+  for (let i = 0; i < ads.length; i++) {
+    ads[i].parentNode.parentNode.style.background = color;
+  }
 }
 
 function setTitle(fallback) {
@@ -40,26 +71,26 @@ function setTitle(fallback) {
 }
 
 function addOsmLink(searchTerm) {
-  const menubar = document.getElementsByClassName("categories")[0];
-  menubar.className = "categories"; // removes additional styling
-
-  const dropdownDisplay = document.querySelector(".dropdown-display");
-
-  const osmLink = document.createElement("a");
-  osmLink.style = "text-decoration: inherit; color:inherit; white-space: nowrap;";
-  osmLink.href = "https://www.openstreetmap.org/search?query=" + searchTerm;
+  const dropdownDisplay = document.querySelector(".maps").querySelector(".dropdown-display");
 
   // last Dropdown-Entry is empty div tag, like they were waiting for me to fix the extension and add OpenStreetMaps ;)
-  const osmaps = dropdownDisplay.childNodes[dropdownDisplay.childElementCount-2].cloneNode(true);
+  const osmaps = dropdownDisplay.childNodes[2].cloneNode(true);
+
   osmaps.querySelector("span").textContent = "OpenStreetMap";
   osmaps.querySelector("img").src = "https://icon.horse/icon/openstreetmap.com";
-  osmLink.appendChild(osmaps);
-  dropdownDisplay.appendChild(osmLink);
+  osmaps.onclick = function() { window.open("https://www.openstreetmap.org/search?query=" + searchTerm, '_blank'); };
+  dropdownDisplay.appendChild(osmaps);
 }
 
-var intervalId = window.setInterval(function() {
-  if (document.querySelectorAll("[src*='icon.horse']").length === 0) {
-    console.log("Enhancing Startpage...");
-    enhance();
+(async () => {
+  console.log("Enhancing Startpage...");
+  const searchTerm = enhanceOnLoad();
+
+  if (searchTerm !== undefined && searchTerm !== "") {
+    window.setInterval(function() {
+      if (document.querySelectorAll("[src*='icon.horse']").length === 0) {
+        enhanceDelayed(searchTerm);
+      }
+    }, 500);
   }
-}, 100);
+})();
